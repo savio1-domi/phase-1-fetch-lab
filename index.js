e16838d45f4cad26d04d5cd13ea86a0ce6a8de9c
git-savio1.domi <techsavvydominic3@gmail.com>
2023-03-23 12:10:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+
+  // Sending a fetch request
   const fetchGame = fetch('https://anapioficeandfire.com/api/books');
   return fetchGame.then(res => {return res.json()}).then(data => {renderBooks(data)})
 
